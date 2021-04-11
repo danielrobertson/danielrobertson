c99539aaa27d24a719d1eb95ded2e278867e2a55
Daniel Robertson <danielrobertson733@gmail.com>
2021-04-11 16:43:16 -0500

    simplifhy footer css, render projects wip

--- pages/index.js
+++ pages/index.js
@@ -33,8 +33,15 @@ const socialLinks = Object.freeze([
   "email"
 ]);
 
+const NUMBER_OF_PREVIEW_PROJECTS = 3;
+
 export default function Home({ user, projects }) {
-  const { image, name, shortBio, title } = user;
+  const { image, name, shortBio, title, github } = user;
+
+  const projectsToPreview = projects.slice(
+    0,
+    NUMBER_OF_PREVIEW_PROJECTS
+  );
 
   return (
     <>
@@ -47,25 +54,25 @@ export default function Home({ user, projects }) {
           height: 100%;
         }
       `}</style>
-      <div className="flex flex-col h-full">
+      <div className="flex flex-col min-h-full">
         <Head>
-          <title>Daniel Robertson</title>
+          <title>{name}</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <main id="top" className="flex-grow flex-shrink-0 flex-">
+        <main id="top" className="">
           <header className="flex flex-col items-center bg-green text-gray-lightest pb-14">
             <img
               src={image.fields.file.url}
               className="rounded-full mt-16 h-52 w-52 text-xl"
               alt="headshot"
             />
-            <div className="text-xl mt-4">{user.title}</div>
+            <div className="text-xl mt-4">{title}</div>
 
             <nav className="mt-8 text-5xl font-thin">
               <ol className="flex">
                 {navigationLinks.map((link) => (
-                  <li className="mx-6">
+                  <li key={link.label} className="mx-6">
                     <Link href={link.path}>
                       <a href={link.path}>{link.label}</a>
                     </Link>
@@ -88,36 +95,48 @@ export default function Home({ user, projects }) {
             <a id="about" href="#about">
               <h2 className="text-6xl">About</h2>
             </a>
-            <div className="px-16 mt-10">{user.shortBio}</div>
+            <div className="px-16 mt-10">{shortBio}</div>
           </section>
           <section className="mt-20 text-center text-gray-dark">
             <a id="projects" href="#projects">
               <h2 className="text-6xl">Projects</h2>
             </a>
             <ul className="mt-10 text-xl list-none">
-              {projects.map((p) => (
-                <li key={p.name}>{p.name}</li>
-              ))}
+              {projectsToPreview.map(
+                ({ name: projectName, images }) => {
+                  const {
+                    fields: { file: thumbnail }
+                  } = images.find(
+                    ({ fields }) => fields.title === "thumbnail"
+                  );
+
+                  return (
+                    <li key={projectName}>
+                      <div className="">{projectName}</div>
+                      <img
+                        src={thumbnail.url}
+                        alt={`${projectName} thumbnail`}
+                      />
+                    </li>
+                  );
+                }
+              )}
             </ul>
           </section>
           <section className="mt-20 text-center text-gray-dark">
             <a id="contact" href="#contact">
               <h2 className="text-6xl">Contact</h2>
             </a>
-            hmu on snapchat 😜
+            snapchats still cool amirite 😜 🤙🏼 🥵
           </section>
-          <div className="mt-20 text-gray-light text-center">
+          <div className="my-20 text-gray-light text-center">
             <a href="#top">Back to Top</a>
           </div>
-          <div className="push" />
         </main>
 
-        <footer className="flex-shrink-0 text-center mt-16 py-16 text-gray-lightest w-full bg-blue-dark">
+        <footer className="mt-auto text-center py-16 text-gray-lightest w-full bg-blue-dark">
           Made in Austin 📍 by{" "}
-          <a
-            className="underline"
-            href={`${user.github}/danielrobertson`}
-          >
+          <a className="underline" href={`${github}/danielrobertson`}>
             danielrobertson
           </a>{" "}
         </footer>
@@ -135,7 +154,7 @@ export async function getStaticProps() {
   const projects = projectsResponse.items.map((p) => p.fields);
   console.log(
     "🚀 ~ file: index.js ~ line 51 ~ getStaticProps ~ projects",
-    projects
+    JSON.stringify(projects, null, 2)
   );
   console.log(
     "🚀 ~ file: index.js ~ line 49 ~ getStaticProps ~ userResponse",
