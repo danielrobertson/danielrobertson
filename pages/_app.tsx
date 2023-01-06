8107ffa5dca112b8d3eae1635e0e94ccf5aaf0fb
Daniel Robertson <danielrobertson733@gmail.com>
2023-01-06 16:02:29 -0600

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
+import { Analytics } from "@vercel/analytics/react";
 import "tailwindcss/tailwind.css";
 
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>;
 }
 
 export default MyApp;
