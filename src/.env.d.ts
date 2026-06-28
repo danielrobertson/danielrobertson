b5ca744fe7ae2bb271ca80920d2d1d74682fbe39
Daniel Robertson <danielrobertson733@gmail.com>
2026-06-27 20:04:48 -0500

    refactor: deprecate Contentful CMS integration in favor of static data

--- src/.env.d.ts
+++
@@ -1,6 +0,0 @@
-interface ImportMetaEnv {
-  readonly CONTENTFUL_SPACE_ID: string;
-  readonly CONTENTFUL_DELIVERY_TOKEN: string;
-  readonly CONTENTFUL_PREVIEW_TOKEN: string;
-  readonly CONTENTFUL_USER: string;
-}
